4df0e2f8d9bfdab1fe91c4c80ddcb410818893a8
Sam <armandawoodkhan@gmail.com>
2025-07-20 17:26:52 +0530

    Assignment Completed

--- relayer-ws/index.ts
+++ relayer-ws/index.ts
@@ -1,18 +1,38 @@
-import { WebSocketServer, WebSocket } from 'ws';
+import { WebSocketServer, WebSocket as WebSocketWsType } from 'ws';
 
 const wss = new WebSocketServer({ port: 3001 });
 
-const servers: WebSocket[] = [];
-
+const servers: WebSocketWsType[] = [];
+const subscribedRoom = new Map<WebSocketWsType,Set<string>>();
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   servers.push(ws);
 
   ws.on('message', function message(data: string) {
-    servers.map(socket => {
-        socket.send(data);
-    })
-  });
+    const parsedData = JSON.parse(data.toString());
+    const room = parsedData.room;
+    
+    if (parsedData.type == "SUBSCRIBE") {
+      if (!subscribedRoom.has(ws)) {
+        
+        subscribedRoom.set(ws,new Set())
+      }
+      subscribedRoom.get(ws)!.add(room)
+    }
+    if (parsedData.type == "chat") {
+      servers.map(socket => {
+        if (subscribedRoom.get(socket)?.has(room)) {      
+          socket.send(data.toString());
+        }
+      })
+    }
+    if (parsedData.type == "UNSUBSCRIBE") {
+      subscribedRoom.get(ws)?.delete(room);
+      if (subscribedRoom.get(ws)?.size==0) {
+        subscribedRoom.delete(ws);
+      }
+    }
 
+  });
 });
